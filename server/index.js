fdfda996f5c866e022c56b5d089e5771d220dced
jml556 <jmleesjc2000@gmail.com>
2022-02-12 15:02:17 -0500

    cleaned up code for pull request and added comments
    cleaned up code for pull request and added comments

--- server/index.js
+++ server/index.js
@@ -1,26 +1,29 @@
-const express = require('express')
-const euclid = require('./public/data')
-const getBookAndProp = require('./utility/parser')
-const path = require('path')
+const express = require("express");
+const euclid = require("./public/data");
+const path = require("path");
+const PORT = 4000;
 
-const PORT = 4000
+const app = express();
 
-const app = express()
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', '/index.html'));
-})
+//listens for get requests on the root directory
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "/index.html"));
+});
 
 //REST api handler for book and prop
-app.get('/book/:book/prop/:prop', (req, res) => {
-  const propLocation = req.params
-  const prop = euclid['book' + propLocation.book]['prop' + propLocation.prop]
-  Object.assign(prop, {'book': propLocation.book, 'proposition': propLocation.prop})
+app.get("/book/:book/prop/:prop", (req, res) => {
+  const propLocation = req.params;
+  const prop = euclid["book" + propLocation.book]["prop" + propLocation.prop];
 
-  res.json(prop)
-})
+  Object.assign(prop, {
+    book: propLocation.book,
+    proposition: propLocation.prop,
+  });
 
-//listen on localhost with PORT 
+  res.json(prop);
+});
+
+//listen on localhost with PORT
 app.listen(PORT, () => {
-  console.log(`app is listening on ${PORT}`)
-})
+  console.log(`app is listening on ${PORT}`);
+});
--- server/utility/parser.js
+++ server/utility/parser.js
@@ -1,18 +1,7 @@
+//File for potential utility functions
 function getBookAndProp(obj) {
-  const {book, prop} = obj
-  return {book: book.split('book')[1], prop: prop.split('prop')[1]}
+  const { book, prop } = obj;
+  return { book: book.split("book")[1], prop: prop.split("prop")[1] };
 }
 
-const getData = async () => {try {
-    const response = await fetch('http://localhost:4000/book/2/prop/3')
-    const data = await response.json()
-    console.log(data)
-  }
-
-  catch(e) {
-    console.log(e, 'data could not be fetched')
-  }
-}
-
-module.exports = getBookAndProp
-
+module.exports = getBookAndProp;
