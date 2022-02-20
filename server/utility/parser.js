f7f15a4f000de1d2c01c0ca7a3d20c27d604f20d
Joe <jmleesjc2000@gmail.com>
2022-02-20 13:12:06 -0500

    added app for front end

--- server/utility/parser.js
+++
@@ -1,7 +0,0 @@
-//File for potential utility functions
-function getBookAndProp(obj) {
-  const { book, prop } = obj;
-  return { book: book.split("book")[1], prop: prop.split("prop")[1] };
-}
-
-module.exports = getBookAndProp;
